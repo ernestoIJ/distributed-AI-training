c4d5c74849aed86fc16fd84a553b33c6f11f5d95
ErnestoIJ <ibanezej10@gmail.com>
2024-07-25 20:40:08 -0700

    Update mnist_arch_model.py

--- backend/mnist_arch_model.py
+++ backend/mnist_arch_model.py
@@ -4,10 +4,12 @@ import torch.nn.functional as F
 class MNISTModel(nn.Module):
   def __init__(self, input_shape: int, output_shape: int, hidden_units: int):
     super(MNISTModel, self).__init__()
+    self.flatten = nn.Flatten()
     self.fc1 = nn.Linear(in_features=input_shape, out_features=hidden_units)
     self.fc2 = nn.Linear(in_features=hidden_units, out_features=output_shape)
 
   def forward(self, x):
-    x = F.relu(self.fc1(x.view(-1, 28*28)))
+    x = self.flatten(x)
+    x = F.relu(self.fc1(x))
     x = self.fc2(x)
     return x
